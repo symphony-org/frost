4c0a414f27ca39429efe3affda6618b2ec6ea903
Pawel Szulc <paul.szulc@gmail.com>
2019-07-24 00:20:14 +0200

    Include error in dynamic data creation

--- src/Frost.hs
+++ src/Frost.hs
@@ -4,15 +4,20 @@ module Frost where
 import Polysemy
 import Polysemy.Input
 import Polysemy.Output
+import Polysemy.Error
 import Text.Pandoc
 
+
+data DynamicError = DynamicError String
+
 data DynamicContent m a where
-  Transform :: Pandoc -> DynamicContent m Pandoc
+  Transform :: Pandoc -> DynamicContent m (Either DynamicError Pandoc)
 
 makeSem ''DynamicContent
 
 generateDocs :: ( Member (Input Pandoc) r
                 , Member (Output Pandoc) r
                 , Member DynamicContent r
+                , Member (Error DynamicError) r
                 ) => Sem r ()
-generateDocs  = input >>= transform >>= output
+generateDocs  = input >>= transform >>= either throw output
