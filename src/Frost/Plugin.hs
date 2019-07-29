5fa58be31a38cf4d3ad83f2c351a7e7d27adac71
Pawel Szulc <paul.szulc@gmail.com>
2019-07-29 12:29:10 +0200

    Add name and substitute functions to Plugin

--- src/Frost/Plugin.hs
+++ src/Frost/Plugin.hs
@@ -4,5 +4,7 @@ import Text.Pandoc
 import Polysemy
 
 data Plugin r = Plugin {
+                     name :: String,
+                     substitute :: Sem r [Block],
                      addToMeta :: Meta -> Sem r Meta
                      }
