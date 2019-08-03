605f44bc4369d55c2351dcef44425d4bff051a30
Pawel Szulc <paul.szulc@gmail.com>
2019-08-03 10:51:41 +0200

    Extract PandocRun
    PandocRun holds interepreters for both Input Pandoc and Output
    Pandoc. Those interepreters needs to be tested, so that we know
    exactly what Pandoc structire is generated from incoming files and
    also what kind of file is generated from the structure

--- app/Main.hs
+++ app/Main.hs
@@ -2,15 +2,13 @@ module Main where
 
 import Frost
 import Frost.Plugins (transform, plugins)
+import Frost.PandocRun (runInputPandoc, runOutputPandoc)
 
-import Prelude hiding (readFile, writeFile)
 import Data.Function ((&))
 import qualified Data.Text as T
 import System.Exit
 import Text.Pandoc hiding (trace)
 import Polysemy
-import Polysemy.Input
-import Polysemy.Output
 import Polysemy.Error
 import Polysemy.Trace
 import PolysemyContrib
@@ -19,8 +17,8 @@ main :: IO ()
 main =  generate >>= handleErrors
   where
     generate =  generateDocs (transform plugins)
-      & fetchInputFile
-      & writeOutFile
+      & runInputPandoc
+      & runOutputPandoc
       & runFileProviderIO
       & runSystemEffect
       & runTraceIO
@@ -29,31 +27,4 @@ main =  generate >>= handleErrors
       & runM
     handleErrors = either (die.show) (either (die.show) (\_ -> exitSuccess))
 
-fetchInputFile :: (
-    Member (Lift IO) r
-  , Member FileProvider r
-  , Member (Error PandocError) r
-  ) => Sem (Input Pandoc ': r) a -> Sem r a
-fetchInputFile = interpret $ \case
-  Input -> do
-    content <- readFile "documentation.md"
-    fromPandocIO $ readMarkdown settings content
-  where
-    settings = def { readerExtensions = extensionsFromList [Ext_yaml_metadata_block, Ext_backtick_code_blocks]}
 
-writeOutFile :: (
-    Member (Lift IO) r
-  , Member FileProvider r
-  , Member (Error PandocError) r
-  ) => Sem (Output Pandoc ': r) a -> Sem r a
-writeOutFile = interpret $ \case
-  Output pandoc -> do
-    content <- fromPandocIO $ writeHtml4String def pandoc
-    writeFile "documentation.html" content
-
-fromPandocIO :: (
-    PandocMonad PandocIO
-  , Member (Error PandocError) r
-  , Member (Lift IO) r
-  ) => PandocIO a -> Sem r a
-fromPandocIO pioa = fromEitherSem $ sendM $ runIO pioa
