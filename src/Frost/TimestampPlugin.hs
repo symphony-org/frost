b11adf2a1507aa4848ff49af214e2006bb055f62
Pawel Szulc <paul.szulc@gmail.com>
2019-08-03 10:51:34 +0200

    Introduce timestampPlugin & timestampMetaPlugin

--- src/Frost/TimestampPlugin.hs
+++ src/Frost/TimestampPlugin.hs
@@ -1,16 +1,23 @@
 module Frost.TimestampPlugin where
 
 import Frost.Plugin
+import Data.Functor
 import Text.Pandoc
 import Polysemy
 import PolysemyContrib
 import Data.Map.Strict
 
-timestampPlugin :: Member SystemEffect r => Plugin r
-timestampPlugin = Plugin "timestamp:meta" (\_ -> return []) atm
+timestampMetaPlugin :: Member SystemEffect r => Plugin r
+timestampMetaPlugin = Plugin "timestamp:meta" (\_ -> return []) atm
   where
     atm :: Member SystemEffect r =>  Meta -> Sem r Meta
     atm meta = do
       time <- currentTime
       return $ Meta $ (insertTimestamp time) $ unMeta meta
     insertTimestamp t= insert "creation" (MetaString $ show t)
+
+timestampPlugin :: Member SystemEffect r => Plugin r
+timestampPlugin = Plugin "timestamp" sub (\m -> return m)
+  where
+    sub = (\_ ->  currentTime <&> render)
+    render t = [Plain [Str $show t]]
