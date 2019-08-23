d26cbc3060d67c69c998452caf01a767f1087fe3
Pawel Szulc <paul.szulc@gmail.com>
2019-08-23 17:15:30 +0200

    Fix runStackSys intepreter for Stack tests

--- test/StackSpec.hs
+++ test/StackSpec.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TupleSections #-}
 module StackSpec where
 
 import Frost.Effects.Sys
@@ -12,35 +13,40 @@ spec :: Spec
 spec =
   describe "Frost.Effects.Stack runStack" $ do
     it "clean should call stack --no-terminal clean" $ do
-      let result = runStack clean
-                 & runCmd ("stack --no-terminal clean", "cleaned")
+      let result = runStackSys clean
+                 & runCmd ("stack --no-terminal clean", withStdErr "cleaned")
                  & run
       result `shouldBe` "cleaned"
     it "build should call stack --no-terminal build" $ do
-      let result = runStack build
-                 & runCmd ("stack --no-terminal build", "built")
+      let result = runStackSys build
+                 & runCmd ("stack --no-terminal build", withStdErr "built")
                  & run
       result `shouldBe` "built"
     it "exec should call stack --no-terminal exec <<program_name>>" $ do
-      let result = runStack (exec "foo-exe")
-                 & runCmd ("stack --no-terminal exec foo-exe", "result of exec")
+      let result = runStackSys (exec "foo-exe")
+                 & runCmd ("stack --no-terminal exec foo-exe", withStdErr "result of exec")
                  & run
       result `shouldBe` "result of exec"
     it "test should call stack --no-terminal test" $ do
-      let result = runStack test
-                 & runCmd ("stack --no-terminal test", "result of test")
+      let result = runStackSys test
+                 & runCmd ("stack --no-terminal test", withStdOut "result of test")
                  & run
       result `shouldBe` "result of test"
     it "testMatch should call stack --no-terminal test --match \"/<<filename>>/<<specname>>/<<testname>>/\"" $ do
-      let result = runStack (testMatch "boo" "foo" "bar")
-                 & runCmd ("stack --no-terminal test --match \"/boo/foo/bar/\"", "result of test")
+      let result = runStackSys (testMatch "boo" "foo" "bar")
+                 & runCmd ("stack --no-terminal test --match \"/boo/foo/bar/\"", withStdOut "result of test")
                  & run
       result `shouldBe` "result of test"
 
-runCmd :: (String, String) -> Sem (Sys ': r) a -> Sem r a
+runCmd :: (String, (String, String)) -> Sem (Sys ': r) a -> Sem r a
 runCmd (arg, response) program = runSysPure ts func program
   where
     ts = undefined
-    func str = if str == arg then ("", response) else ("", "")
-  
+    func str = if str == arg then response else ("", "")
+
+withStdOut :: String -> (String, String)
+withStdOut = (, "")
+
+withStdErr :: String -> (String, String)
+withStdErr = ("", )
 
