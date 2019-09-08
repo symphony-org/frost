e9ca8a2f14014c9ccac878b8d6e5e37c6326275e
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2019-09-08 21:38:10 +0200

    Add python effect

---
+++ src/Frost/Effects/Python.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Frost.Effects.Python where
+
+import Frost.Effects.Sys
+import Polysemy
+
+data Python m a where
+  Exec :: String -> Python m String
+
+makeSem ''Python
+
+runPython :: Member Sys r => Sem (Python ': r) a -> Sem r a
+runPython = interpret $ \case
+  Exec script -> showStdOut (cmd $ "python -c '" ++ script ++ "'")
+
+showStdOut :: Sem r (String, String) -> Sem r String
+showStdOut = fmap fst
---
+++ test/PythonSpec.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE TupleSections #-}
+module PythonSpec where
+
+import Frost.Effects.Sys
+import Frost.Effects.Python
+
+import Polysemy
+
+import Data.Function               ((&))
+import Test.Hspec
+
+spec :: Spec
+spec =
+  describe "Frost.Effects.Python runPython" $ do
+    it "run script should call 'python -c script'" $ do
+      let result = runPython (exec "script")
+                 & runCmd ("python -c 'script'", withStdOut "executed a python script")
+                 & run
+      result `shouldBe` "executed a python script"
+
+runCmd :: (String, (String, String)) -> Sem (Sys ': r) a -> Sem r a
+runCmd (arg, response) program = runSysPure ts func program
+  where
+    ts = undefined
+    func str = if str == arg then response else ("", "")
+
+withStdOut :: String -> (String, String)
+withStdOut = (, "")
