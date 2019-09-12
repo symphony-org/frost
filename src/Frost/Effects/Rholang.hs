0f97ef7ff72083fd6daafc5e910af7df7e9ad67a
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2019-09-12 22:47:12 +0200

    Add Rholang effect

---
+++ src/Frost/Effects/Rholang.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Frost.Effects.Rholang where
+
+import Frost.Effects.Sys
+import Polysemy
+
+data Rholang m a where
+  Exec :: String -> Rholang m String
+
+makeSem ''Rholang
+
+runRholang :: Member Sys r => Sem (Rholang ': r) a -> Sem r a
+runRholang = interpret $ \case
+  Exec script -> showStdOut (cmd $ "rholang '" ++ script ++"'")
+  where
+    showStdOut :: Sem r (String, String) -> Sem r String
+    showStdOut = fmap fst
