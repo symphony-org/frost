878feb04d2d047cefee332589cfb0b1a42253002
Łukasz Gołębiewski <lukasz.golebiewski@gmail.com>
2019-11-13 18:36:24 +0100

    Add first integration test - PythonSpec

---
+++ integration-test/PythonSpec.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE TupleSections #-}
+module PythonSpec where
+
+import Frost.Effects.Sys
+import Frost.Effects.Python
+import FrostError
+
+import Polysemy
+import Polysemy.Error
+
+import Data.Function               ((&))
+import Test.Hspec
+
+spec :: Spec
+spec =
+  describe "Frost.Effects.Python runPython" $ do
+    it "should execute python" $ do
+      result <- runPython (exec "print(1+3)")
+                & runSysIO
+                & runError @FrostError
+                & runM
+      result `shouldBe` Right "4\n"
