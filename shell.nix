faee685c75f7fad7d7d6ad2c8f1e68053355c176
Pawel Szulc <paul.szulc@gmail.com>
2020-06-12 09:36:16 +0200

    Fix stack nix osx (#68)
    * Fix Nix integration on OSX
    
    Followed discussion on
    https://github.com/commercialhaskell/stack/issues/1698 which led to
    https://github.com/idris-lang/Idris-dev/pull/2938
    
    * Add workaround for cabal/4739 issue

--- shell.nix
+++ shell.nix
@@ -4,5 +4,10 @@ with (import <nixpkgs> {});
 haskell.lib.buildStackProject {
   inherit ghc;
   name = "frost";
-  buildInputs = [ python3 zlib ghcid ];
+  # Stack with Nix enabled does not work on OSX (:shrug:)
+  # Workaround that I found on Idris https://github.com/idris-lang/Idris-dev/pull/2938 fixes the issue
+  buildInputs = [ python3 zlib ghcid ] ++ lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
+    Cocoa
+    CoreServices
+  ]);
 }
